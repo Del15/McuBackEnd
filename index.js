ba4e898254d040ba649e9b261ffb6d8120a045c5
D Ellis <142749333+Del15@users.noreply.github.com>
2024-02-01 07:56:30 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+// const path = require('path');
+const logger = require('morgan')
+const connectToMongoDB = require('./db/mongodb');
+require('dotenv').config();
+
+// Prevent CORS issue
+const cors = require('cors');
+
+// Give access to any origin
+const corsOptions = {
+    origin: "*",
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+
+
+// Read incoming requests properly
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// logs requests to the server
+app.use(logger('dev'))
+
+
+const McuRouter = require('./routes/McuRouter');
+// localhost:3001/Mcu/...
+app.use('/Mcu', McuRouter);
+
+
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`server listening on port ${PORT}`);
+
+    connectToMongoDB();
+});
